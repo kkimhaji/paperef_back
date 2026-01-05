9a7828f866da28bedc99953b713f37e3d6364ffd
kkimhaji <kkim.haji@gmail.com>
2026-01-05 13:59:47 +0900

    [chore] Update database name to paperef_db

--- app/database.py
+++ app/database.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql://postgres:postgres@localhost:5432/paper_memo_db"
+    "postgresql://postgres:postgres@localhost:5432/paperef_db"
 )
 
 engine = create_engine(DATABASE_URL)
